237ed8fc430e584c9bf73047d53f0cd84e83fcaf
Surya Narayana <suryakrish31@gmail.com>
2024-04-13 21:19:15 +0530

    Added Readme. Project_I2C in progress.

---
+++ Project_I2C/top.v
@@ -0,0 +1,13 @@
+`timescale 1ns / 1ps
+///////////////////////////////////////////////////////////
+// Project Code		:	JP_24001
+// Author			:	Surya Narayana Rangavajhala
+// 
+// Creation Date 	:	13.04.2024 20:44:00
+// Design Name		:	I2C Protocol
+// Version			:	1.0.0
+// Target Device	:	None
+// Tool Version		:	Standalone source
+// Description		:	Implementation of I2C Protocol for Job Prep
+///////////////////////////////////////////////////////////
+
---
+++ README.md
@@ -0,0 +1,9 @@
+# RTL Projects
+1. This repo contains all the RTL Projects (Sources only) that I have created.
+2. Each project is named with a *unique project code.*
+3. Below are the list of the current projects.
+
+
+|	**$${\color{blue}Project Code}$$**	|	**$$(\color(blue)Description)$$**	|
+|	:---	|	:---:	|
+|	JP_24001	|	Implementation of I2C Protocol for Job Prep	|
